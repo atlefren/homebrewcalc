b6ab2b176742cbfafb5962adee1ca21cf857be88
Atle Frenvik Sveen <atlsve@norkart.no>
2019-02-25 11:52:14 +0100

    add conversions

--- src/conversions.js
+++ src/conversions.js
@@ -1,1 +1,5 @@
 export const gramsToLbs = grams => grams * 0.002205;
+
+export const litersToGallons = liters => liters * 0.264172051242;
+
+export const srmToEbc = srm => Math.round(srm * 1.97);
--- src/conversions.test.js
+++ src/conversions.test.js
@@ -1,10 +1,24 @@
-import {gramsToLbs} from './conversions';
+import {
+    gramsToLbs,
+    litersToGallons,
+    srmToEbc
+} from './conversions';
 
 describe('gramsToLbs', () => {
-
     it('converts 1 gram to lbs', () => {
-
         expect(gramsToLbs(1)).toEqual(0.002205);
     });
+});
 
+
+describe('litersToGallons', () => {
+    it('converts 1 liter to gallons', () => {
+        expect(litersToGallons(1)).toEqual(0.264172051242);
+    });
+});
+
+describe('srmToEbc', () => {
+    it('converts 5 srm to ebc', () => {
+        expect(srmToEbc(5)).toEqual(10);
+    });
 });
