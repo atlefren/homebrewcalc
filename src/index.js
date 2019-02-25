125a51c384f7600d6ceaf0d05ab6be6cd2889aef
Atle Frenvik Sveen <atlsve@norkart.no>
2019-02-25 15:06:54 +0100

    add index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import computeAbv from './computations/computeAbv';
+import computeBitterness from './computations/computeBitterness';
+import computeColor from './computations/computeColor';
+import computeEfficiency from './computations/computeEfficiency';
+import computeFG from './computations/computeFG';
+import computeGravity from './computations/computeGravity';
+
+
+export {
+    computeAbv,
+    computeBitterness,
+    computeColor,
+    computeEfficiency,
+    computeFG,
+    computeGravity
+};
